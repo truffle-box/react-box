3ba11540d216023690c8fc8cc9f528478460cd11
jquintalzcg <Creative_1280!>
2017-03-06 02:41:54 -0500

    Cleanup of unused vars.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import truffleConfig from './../truffle-config.js'
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
