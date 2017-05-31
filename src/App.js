e14bff7692af353c1aa0a1bdbe84bbf4fc78101f
Unknown <joshquin@gmail.com>
2017-05-31 15:18:10 -0400

    Grammar cleanup in index.

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
       simpleStorage.deployed().then((instance) => {
         simpleStorageInstance = instance
 
-        // Stores a value of 5.
+        // Stores a given value, 5 by default.
         return simpleStorageInstance.set(5, {from: accounts[0]})
       }).then((result) => {
         // Get the value from the contract to prove it worked.
@@ -82,8 +82,8 @@ class App extends Component {
               <h1>Good to Go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
               <h2>Smart Contract Example</h2>
-              <p>The below will show a stored value of 5 by default if your contracts compiled and migrated successfully.</p>
-              <p>Try changing the value stored on <strong>line 50</strong> of App.js.</p>
+              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
+              <p>Try changing the value stored on <strong>line 61</strong> of App.js.</p>
               <p>The stored value is: {this.state.storageValue}</p>
             </div>
           </div>
