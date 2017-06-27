8e556bee35cf535e818afac3af89a5b67ce6b2be
RStorm <rstormsf@gmail.com>
2017-06-26 21:36:07 -0700

    Fix detection of web3 and use state variable

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
               <h1>Good to Go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
               <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
+              <p>If your contracts compiled and migrated successfully, below will show a stored value of {this.state.storageValue} (by default).</p>
               <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
               <p>The stored value is: {this.state.storageValue}</p>
             </div>
