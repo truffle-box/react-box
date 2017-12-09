dc9c5d063baa80088ddbc49f732886605c09ac3d
groxni <nishukai@hotmail.com>
2017-12-09 12:48:51 +0800

    Update test and add Codecov

--- scripts/test.js
+++ scripts/test.js
@@ -15,17 +15,4 @@ if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
 
-// A temporary hack to clear terminal correctly.
-// You can remove this after updating to Jest 18 when it's out.
-// https://github.com/facebook/jest/pull/2230
-var realWrite = process.stdout.write;
-var CLEAR = process.platform === 'win32' ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H';
-process.stdout.write = function(chunk, encoding, callback) {
-  if (chunk === '\x1B[2J\x1B[H') {
-    chunk = CLEAR;
-  }
-  return realWrite.call(this, chunk, encoding, callback);
-};
-
-
 jest.run(argv);
