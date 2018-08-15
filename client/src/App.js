29e435b057786487cff60befbcd2b7d4c12331f6
Adrian Li <li.adrianmc@gmail.com>
2018-08-15 15:53:41 -0400

    use truffle-contract for method calling and contract instantiation

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
-import getContractInstance from "./utils/getContractInstance";
+import truffleContract from "truffle-contract";
 
 import "./App.css";
 
@@ -16,12 +16,14 @@ class App extends Component {
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance by passing in web3 and the contract definition.
-      const contract = await getContractInstance(web3, SimpleStorageContract);
+      // Get the contract instance.
+      const Contract = truffleContract(SimpleStorageContract);
+      Contract.setProvider(web3.currentProvider);
+      const instance = await Contract.deployed();
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,13 +37,13 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    await contract.set(5, { from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call({ from: accounts[0] });
+    const response = await contract.get();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ storageValue: response.toNumber() });
   };
 
   render() {
