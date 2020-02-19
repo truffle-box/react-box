036a742be1149ae40430127b98b3f5e0c9140867
Yasir Jafar <yasir991925@gmail.com>
2020-02-20 04:03:17 +0530

    Removing window.addEventListener('load',async () ....
    this change goes with https://github.com/truffle-box/react-box/pull/117
    the provide the other change that has an effect on this file
    async function doesn't work property with useEffect on mount
    ( and this is the reason for this change and goes with the change mentioned in the link )

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -3,13 +3,12 @@ import Web3 from "web3";
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
-    window.addEventListener("load", async () => {
       // Modern dapp browsers...
       if (window.ethereum) {
         const web3 = new Web3(window.ethereum);
         try {
           // Request account access if needed
-          await window.ethereum.enable();
+          window.ethereum.enable();
           // Acccounts now exposed
           resolve(web3);
         } catch (error) {
@@ -32,7 +31,6 @@ const getWeb3 = () =>
         console.log("No web3 instance injected, using Local web3.");
         resolve(web3);
       }
-    });
   });
 
 export default getWeb3;
