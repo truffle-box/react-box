5ebcb6bd4e3182e5f107a55655b9038755a09bf2
David <sharp2448@seznam.cz>
2021-07-20 18:39:50 +0200

    SimpleStorage.sol updated to newest version (>=0.4.22 <0.9.0)

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract SimpleStorage {
-  uint storedData;
+    uint256 storedData;
 
-  function set(uint x) public {
-    storedData = x;
-  }
+    function set(uint256 x) public {
+        storedData = x;
+    }
 
-  function get() public view returns (uint) {
-    return storedData;
-  }
+    function get() public view returns (uint256) {
+        return storedData;
+    }
 }
