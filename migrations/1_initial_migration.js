9295ce3aeedf75b0780cccddda97c7a7833405d5
David <sharp2448@seznam.cz>
2021-07-20 18:42:31 +0200

    Migration JS file updated to newest version standart

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+const Migrations = artifacts.require('Migrations');
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+    deployer.deploy(Migrations);
 };
