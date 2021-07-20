3d72e45ec786ad43477c7cae2a8a91723b740e33
David <sharp2448@seznam.cz>
2021-07-20 17:40:35 +0200

    migration.sol updated to newest version (>=0.4.22 <0.9.0)

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+    address public owner = msg.sender;
+    uint256 public last_completed_migration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
 
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
 }
