6afbc59cc33b22b5af08bba5b7949fa289b411bc
cds-amal <cds.sudama@gmail.com>
2021-10-14 20:30:29 -0400

    update getWeb3

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -1,38 +1,21 @@
 import Web3 from "web3";
 
-const getWeb3 = () =>
-  new Promise((resolve, reject) => {
-    // Wait for loading completion to avoid race conditions with web3 injection timing.
-    window.addEventListener("load", async () => {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          // Accounts now exposed
-          resolve(web3);
-        } catch (error) {
-          reject(error);
-        }
+const getWeb3 = async () => 
+  new Promise((resolve, reject) => 
+    window.addEventListener('load', async () => {
+      if (typeof window.ethereum !== 'undefined') {
+        console.log('MetaMask is installed!');
+        //Request access to accounts
+        window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        resolve(new Web3(window.ethereum));
+      } else {
+        console.log('Please install MetaMask https://metamask.io');
+        reject(new Error("MetaMask not installed"))
       }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-        const web3 = window.web3;
-        console.log("Injected web3 detected.");
-        resolve(web3);
-      }
-      // Fallback to localhost; use dev console port by default...
-      else {
-        const provider = new Web3.providers.HttpProvider(
-          "http://127.0.0.1:8545"
-        );
-        const web3 = new Web3(provider);
-        console.log("No web3 instance injected, using Local web3.");
-        resolve(web3);
-      }
-    });
-  });
+    })
+  )
+
+
 
 export default getWeb3;
