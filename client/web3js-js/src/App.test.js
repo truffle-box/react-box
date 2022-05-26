5a354ee80bc0b5b9426f09c7b97b66a505f587db
Cliff Zhang <cliffzhang08@gmail.com>
2022-05-26 11:45:22 -0400

    web3js-js: Removed CRA asset, markup, style

--- client/web3js-js/src/App.test.js
+++ client/web3js-js/src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
