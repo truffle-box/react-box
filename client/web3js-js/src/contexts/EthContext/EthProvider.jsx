ca1822644b738a87538101ff51238237a7d59a6b
Cliff Zhang <cliffzhang08@gmail.com>
2022-05-27 13:56:36 -0400

    web3js-js: Wrap `EthProvider` ops

--- client/web3js-js/src/contexts/EthContext/EthProvider.jsx
+++ client/web3js-js/src/contexts/EthContext/EthProvider.jsx
@@ -6,22 +6,22 @@ import { reducer, actions, initialState } from "./state";
 function EthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const initWeb3 = () => {
-    const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-    dispatch({ type: actions.setWeb3, data: web3 });
-  };
-
-  const initAccounts = async () => {
-    const accounts = await state.web3.eth.requestAccounts();
-    dispatch({ type: actions.setAccounts, data: accounts });
+  const ops = {
+    initWeb3: () => {
+      const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+      dispatch({ type: actions.setWeb3, data: web3 });
+    },
+    initAccounts: async () => {
+      const accounts = await state.web3.eth.requestAccounts();
+      dispatch({ type: actions.setAccounts, data: accounts });
+    }
   };
 
   return (
     <EthContext.Provider value={{
       state,
       dispatch,
-      initWeb3,
-      initAccounts
+      ...ops
     }}>
       {children}
     </EthContext.Provider>
