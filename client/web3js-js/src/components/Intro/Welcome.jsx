1d584179e66b1b89b6db9e716b1bb58f29f3cdeb
Cliff Zhang <cliffzhang08@gmail.com>
2022-06-01 14:09:09 -0400

    web3js-js: Intro component

---
+++ client/web3js-js/src/components/Intro/Welcome.jsx
@@ -0,0 +1,14 @@
+function Welcome() {
+  return (
+    <div className="welcome">
+      <h1>👋 Welcome to the Truffle + React Box!</h1>
+      <p>
+        This is everything you need to start using Truffle to write,
+        compile, test, and deploy smart contracts, and interact with
+        them from a React app.
+      </p>
+    </div>
+  );
+}
+
+export default Welcome;
