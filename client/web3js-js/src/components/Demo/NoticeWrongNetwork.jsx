10ccdc2ec78a449878ab15c4bf80e3e2425ddce3
Cliff Zhang <cliffzhang08@gmail.com>
2022-06-02 13:18:41 -0400

    web3js-js: Fix `NoticeWrongNetwork` wording

--- client/web3js-js/src/components/Demo/NoticeWrongNetwork.jsx
+++ client/web3js-js/src/components/Demo/NoticeWrongNetwork.jsx
@@ -1,7 +1,7 @@
 function NoticeWrongNetwork() {
   return (
     <p>
-      ⚠️ MetaMask is not connected to the same network as the one you deployed contracts to.
+      ⚠️ MetaMask is not connected to the same network as the one you deployed to.
     </p>
   );
 }
