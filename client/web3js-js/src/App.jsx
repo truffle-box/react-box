c402c82bce1481b831942b638b6ebfd76cdf8b29
Cliff Zhang <cliffzhang08@gmail.com>
2022-06-02 09:44:51 -0400

    web3js-js: Add everything to `App` component

--- client/web3js-js/src/App.jsx
+++ client/web3js-js/src/App.jsx
@@ -1,10 +1,23 @@
 import { EthProvider } from "./contexts/EthContext";
+import Intro from "./components/Intro/";
+import Setup from "./components/Setup";
+import Demo from "./components/Demo";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <EthProvider>
       <div id="App" >
+        <div className="container">
+          <Intro />
+          <hr />
+          <Setup />
+          <hr />
+          <Demo />
+          <hr />
+          <Footer />
+        </div>
       </div>
     </EthProvider>
   );
