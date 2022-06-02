c333ba4b3d4dcd4dac89241e0a0f538e95628a50
Cliff Zhang <cliffzhang08@gmail.com>
2022-06-02 13:26:05 -0400

    web3js-js: Extract init, chain / account change

--- client/web3js-js/src/contexts/EthContext/EthProvider.jsx
+++ client/web3js-js/src/contexts/EthContext/EthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useCallback, useEffect } from "react";
 import Web3 from "web3";
 import EthContext from "./EthContext";
 import { reducer, actions, initialState } from "./state";
@@ -6,27 +6,45 @@ import { reducer, actions, initialState } from "./state";
 function EthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    const tryInit = async () => {
-      try {
-        const artifact = require("../../contracts/SimpleStorage.json");
+  const init = useCallback(
+    async artifact => {
+      artifact = artifact || state.artifact;
+      if (artifact) {
         const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
         const accounts = await web3.eth.requestAccounts();
         const networkID = await web3.eth.net.getId();
         const { abi } = artifact;
-        const address = artifact.networks[networkID].address;
-        const contract = new web3.eth.Contract(abi, address);
+        let address, contract;
+        try {
+          address = artifact.networks[networkID].address;
+          contract = new web3.eth.Contract(abi, address);
+        } catch (err) {
+          console.error(err);
+        }
         dispatch({
           type: actions.init,
           data: { artifact, web3, accounts, networkID, contract }
         });
+      }
+    }, [state.artifact]);
+
+  useEffect(() => {
+    const tryInit = async () => {
+      try {
+        const artifact = require("../../contracts/SimpleStorage.json");
+        init(artifact);
       } catch (err) {
         console.error(err);
       }
     };
 
     tryInit();
-  }, []);
+  }, [init]);
+
+  useEffect(() => {
+    window.ethereum.on("chainChanged", init);
+    window.ethereum.on("accountsChanged", init);
+  }, [init]);
 
   return (
     <EthContext.Provider value={{
