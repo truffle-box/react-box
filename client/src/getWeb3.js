2726f1f17db3e2746182001c91ed43f7abb8773b
Abhishek Chauhan <ac12644@gmail.com>
2022-06-13 04:56:26 +0200

    updated packages

--- client/src/getWeb3.js
+++ client/src/getWeb3.js
@@ -9,7 +9,7 @@ const getWeb3 = () =>
         const web3 = new Web3(window.ethereum);
         try {
           // Request account access if needed
-          await window.ethereum.enable();
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
           // Accounts now exposed
           resolve(web3);
         } catch (error) {
