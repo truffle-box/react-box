70714991f6cfbead621dd1829ea4c6fbafa08a2f
beakerfm <beakerfm@protonmail.com>
2022-06-14 23:25:28 -0400

    Update truffle-config.js to include more default options required for ganache-cli to function

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,10 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      port: 8545,
+      host: "127.0.0.1",
+      network_id: "*"
     }
   }
 };
