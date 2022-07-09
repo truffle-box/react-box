10a35f6802611c3632a2ef25d2451d237158b65d
Cliff Zhang <cliffzhang08@gmail.com>
2022-07-09 00:47:51 -0400

    Change `gas` comment in Truffle config
    I believe this is from the trufflesuite.com docs? Could not be verified.

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -65,7 +65,7 @@ module.exports = {
     //   provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraProjectId}`),
     //   network_id: 5,       // Goerli's network id
     //   chain_id: 5,         // Goerli's chain id
-    //   gas: 5500000,        // Gas limit used for deploys. Default is 6721975
+    //   gas: 5500000,        // Gas limit used for deploys.
     //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
     //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets)
