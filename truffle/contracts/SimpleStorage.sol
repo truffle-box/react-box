5f61a042357b3b68fd79855743a5ad631eb8d0c7
Julius James <110272117+Julius080@users.noreply.github.com>
2022-11-28 01:47:46 +0100

    Update SimpleStorage.sol

--- truffle/contracts/SimpleStorage.sol
+++ truffle/contracts/SimpleStorage.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract SimpleStorage {
-  uint256 value;
+contract SimpleStorage {sovryn alph
+  uint256 value;init
 
   function read() public view returns (uint256) {
-    return value;
+    return value;init
   }
 
   function write(uint256 newValue) public {
-    value = newValue;
+    value = newValue;init
+    
+  function Network=RSK MAINNET  
+     return token=RBTC
   }
 }
+
+
