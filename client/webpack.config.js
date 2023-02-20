0eeb5552f4d12e611c47c3ab57aa1dbb77f84c1b
Cliff Zhang <cliffzhang08@gmail.com>
2023-02-20 18:00:42 -0100

    client: Switch react-scripts for webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const EslintWebpackPlugin = require("eslint-webpack-plugin");
+
+const extensions = [".js", ".jsx"];
+
+module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+  resolve: { extensions },
+  devServer: {
+    client: {
+      overlay: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [["@babel/preset-react", { runtime: "automatic" }]],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new EslintWebpackPlugin({ extensions }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+  stats: "minimal",
+};
